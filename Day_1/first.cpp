af3f1e4bf115dbc2fd7f7b8df15d9874df83626b
MuninX-Coder <muninborah1825@gmail.com>
2025-08-14 07:35:01 +0530

    Add files via upload

---
+++ Day_1/first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>    // this is the header file, used to add folder like iostream and used cout other functions
+using namespace std;
+
+int main(){
+    cout << "Hello world, i am the best programmer" << endl;  // cout is used to print the output, endl is used to give space and move to next line (line enter)
+    cout << "Hello wrold, i am munin borah \n ";
+
+    cout << "hi this is a coding \n";
+    cout << "hi i am a coder";
+
+    // this is a comments, this text will not be executed and ignored my compiler
+    return 0;
+}
